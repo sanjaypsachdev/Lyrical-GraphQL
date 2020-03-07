8f32004a42c64b2cd1908dfe486142cce910fd5a
ssachdev <sanjay.sachdev@hotmail.com>
2020-03-07 11:50:26 +0530

    Added react client code for the graphql backend

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://dbUser:sanjay@cluster0-shard-00-00-l04nf.mongodb.net:27017,cluster0-shard-00-01-l04nf.mongodb.net:27017,cluster0-shard-00-02-l04nf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
